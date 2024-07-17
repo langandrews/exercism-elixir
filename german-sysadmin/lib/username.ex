5f8df25f13063bacf1aff606397764e5c614ecae
Andrew Lang <andrew.lang@irrigreen.com>
2024-07-16 20:47:40 -0500

    2nd iteration German Sysadmin

--- german-sysadmin/lib/username.ex
+++ german-sysadmin/lib/username.ex
@@ -1,13 +1,15 @@
 defmodule Username do
   def sanitize([]), do: []
   def sanitize([first | rest]) do
-    case first do
-      char when char >= ?a and char <= ?z or char === ?_ -> [char | sanitize(rest)]
-      ?ä -> ~c"ae" ++ sanitize(rest)
-      ?ö -> ~c"oe" ++ sanitize(rest)
-      ?ü -> ~c"ue" ++ sanitize(rest)
-      ?ß -> ~c"ss" ++ sanitize(rest)
-      _ -> sanitize(rest)
-    end
+    sanitized =
+      case first do
+        ?ä -> ~c"ae"
+        ?ö -> ~c"oe"
+        ?ü -> ~c"ue"
+        ?ß -> ~c"ss"
+        char when char >= ?a and char <= ?z or char === ?_ -> [char]
+        _ -> []
+      end
+    sanitized ++ sanitize(rest)
   end
 end
