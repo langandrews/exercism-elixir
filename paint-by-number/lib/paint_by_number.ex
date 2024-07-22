8a76c58c02fc6b5b1eb5057feafe720a741a4d51
Andrew Lang <andrew.lang@irrigreen.com>
2024-07-22 11:40:00 -0500

    Paint by number exercise

---
+++ paint-by-number/lib/paint_by_number.ex
@@ -0,0 +1,44 @@
+defmodule PaintByNumber do
+  def palette_bit_size(color_count) do
+    log2(color_count, 1)
+  end
+
+  defp log2(n, pow) do
+    if Integer.pow(2, pow) - n >= 0 do
+      pow
+    else
+      log2(n, pow + 1)
+    end
+  end
+
+  def empty_picture() do
+    <<>>
+  end
+
+  def test_picture() do
+    <<0::2, 1::2, 2::2, 3::2>>
+  end
+
+  def prepend_pixel(picture, color_count, pixel_color_index) do
+    bit_size = palette_bit_size(color_count)
+    <<pixel_color_index::size(bit_size), picture::bitstring>>
+  end
+
+  def get_first_pixel(<<>>, _color_count), do: nil
+  def get_first_pixel(picture, color_count) do
+    bit_size = palette_bit_size(color_count)
+    <<value::size(bit_size), _rest::bitstring>> = picture
+    value
+  end
+
+  def drop_first_pixel(<<>>, _color_count), do: <<>>
+  def drop_first_pixel(picture, color_count) do
+    bit_size = palette_bit_size(color_count)
+    <<_value::size(bit_size), rest::bitstring>> = picture
+    rest
+  end
+
+  def concat_pictures(picture1, picture2) do
+    <<picture1::bitstring, picture2::bitstring>>
+  end
+end
