5d47452b5e5c5314e682b2c0a99c6c4e8ae97268
Andrew Lang <andrew.lang@irrigreen.com>
2024-07-22 16:42:45 -0500

    Boutique Inventory exercise

---
+++ boutique-inventory/lib/boutique_inventory.ex
@@ -0,0 +1,30 @@
+defmodule BoutiqueInventory do
+  def sort_by_price(inventory) do
+    Enum.sort_by(inventory, &(&1[:price]), :asc)
+  end
+
+  def with_missing_price(inventory) do
+    Enum.filter(inventory, &(&1[:price] === nil))
+  end
+
+  def update_names(inventory, old_word, new_word) do
+    Enum.map(inventory, fn item ->
+      %{item | name: String.replace(item.name, old_word, new_word)}
+    end)
+  end
+
+  def increase_quantity(item, count) do
+    quantities = Map.new(
+      item.quantity_by_size,
+      fn {size, quantity} ->
+        {size, quantity + count}
+      end)
+    %{item | quantity_by_size: quantities}
+  end
+
+  def total_quantity(item) do
+    Enum.reduce(item[:quantity_by_size], 0, fn {_size, quantity}, acc ->
+      acc + quantity
+    end)
+  end
+end
